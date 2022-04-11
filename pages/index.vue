e17a2f596070c2451e26b27a79c634e2bc503a35
era <admin@smartproperty.kz>
2022-04-11 19:09:46 +0600

    fixed scrollbar menu

--- pages/index.vue
+++ pages/index.vue
@@ -1,167 +1,168 @@
 <template>
   <div>
-    <vue-custom-scrollbar class="scroll-area" :settings="{
-        suppressScrollX: false,
-      }">
-      <TheHeader>
-        <template #actions>
-          <NuxtLink to="/renters" class="btn btn-outline-primary">
-            <span class="d-none d-sm-inline">{{language[current][0]}}</span>
-            <svg
-              class="d-sm-none"
-              xmlns="http://www.w3.org/2000/svg"
-              enable-background="new 0 0 24 24"
-              height="24px"
-              viewBox="0 0 24 24"
-              width="24px"
-              fill="currentColor"
-            >
-              <g><rect fill="none" height="24" width="24" /></g>
-              <g><path d="M11,7L9.6,8.4l2.6,2.6H2v2h10.2l-2.6,2.6L11,17l5-5L11,7z M20,19h-8v2h8c1.1,0,2-0.9,2-2V5c0-1.1-0.9-2-2-2h-8v2h8V19z" /></g>
-            </svg>
-          </NuxtLink>
-        </template>
-        <template #navbar>
-          <LinkScrollTo target-id="about" class="header__nav-link">
-            {{language[current][1]}}
-          </LinkScrollTo>
-          <LinkScrollTo target-id="infrastructure" class="header__nav-link">
-            {{language[current][2]}}
-          </LinkScrollTo>
-          <!--        <LinkScrollTo target-id="plan" class="header__nav-link">-->
-          <!--          План помещений-->
-          <!--        </LinkScrollTo>-->
-        </template>
-      </TheHeader>
-      <main>
-        <section class="hero">
-          <div class="container">
-            <MainSlider class="mb-4" />
-            <div class="row">
-              <div class="col-lg-4 mb-3 mb-lg-0">
-                <div class="panel d-flex h-100">
-                  <div class="flex-shrink-0 pt-1">
-                    <img src="~/assets/img/icons/user-arrow.svg" width="26" height="29" alt="">
-                  </div>
-                  <div class="flex-grow-1 ms-3">
-                    <p class="mb-0 fs-18 fw-medium">
-                      {{language[current][3]}}
-                    </p>
-                  </div>
+    <TheHeader>
+      <template #actions>
+        <NuxtLink to="/renters" class="btn btn-outline-primary">
+          <span class="d-none d-sm-inline">{{language[current][0]}}</span>
+          <svg
+            class="d-sm-none"
+            xmlns="http://www.w3.org/2000/svg"
+            enable-background="new 0 0 24 24"
+            height="24px"
+            viewBox="0 0 24 24"
+            width="24px"
+            fill="currentColor"
+          >
+            <g><rect fill="none" height="24" width="24" /></g>
+            <g><path d="M11,7L9.6,8.4l2.6,2.6H2v2h10.2l-2.6,2.6L11,17l5-5L11,7z M20,19h-8v2h8c1.1,0,2-0.9,2-2V5c0-1.1-0.9-2-2-2h-8v2h8V19z" /></g>
+          </svg>
+        </NuxtLink>
+      </template>
+      <template #navbar>
+        <LinkScrollTo target-id="about" class="header__nav-link">
+          {{language[current][1]}}
+        </LinkScrollTo>
+        <LinkScrollTo target-id="infrastructure" class="header__nav-link">
+          {{language[current][2]}}
+        </LinkScrollTo>
+        <!--        <LinkScrollTo target-id="plan" class="header__nav-link">-->
+        <!--          План помещений-->
+        <!--        </LinkScrollTo>-->
+      </template>
+    </TheHeader>
+    <main>
+      <section class="hero">
+        <div class="container">
+          <MainSlider class="mb-4" />
+          <div class="row">
+            <div class="col-lg-4 mb-3 mb-lg-0">
+              <div class="panel d-flex h-100">
+                <div class="flex-shrink-0 pt-1">
+                  <img src="~/assets/img/icons/user-arrow.svg" width="26" height="29" alt="">
+                </div>
+                <div class="flex-grow-1 ms-3">
+                  <p class="mb-0 fs-18 fw-medium">
+                    {{language[current][3]}}
+                  </p>
                 </div>
               </div>
+            </div>
 
-              <div class="col-lg-4 mb-3 mb-lg-0">
-                <div class="panel d-flex h-100">
-                  <div class="flex-shrink-0 pt-1">
-                    <img src="~/assets/img/icons/store.svg" width="28" height="24" alt="">
-                  </div>
-                  <div class="flex-grow-1 ms-3">
-                    <p class="mb-0 fs-18 fw-medium">
-                      {{language[current][4]}}
-                    </p>
-                  </div>
+            <div class="col-lg-4 mb-3 mb-lg-0">
+              <div class="panel d-flex h-100">
+                <div class="flex-shrink-0 pt-1">
+                  <img src="~/assets/img/icons/store.svg" width="28" height="24" alt="">
+                </div>
+                <div class="flex-grow-1 ms-3">
+                  <p class="mb-0 fs-18 fw-medium">
+                    {{language[current][4]}}
+                  </p>
                 </div>
               </div>
+            </div>
 
-              <div class="col-lg-4 mb-3 mb-lg-0">
-                <div class="panel d-flex h-100">
-                  <div class="flex-shrink-0 pt-1">
-                    <img src="~/assets/img/icons/stock.svg" width="28" height="21" alt="">
-                  </div>
-                  <div class="flex-grow-1 ms-3">
-                    <p class="mb-0 fs-18 fw-medium">
-                      {{language[current][5]}}
-                    </p>
-                  </div>
+            <div class="col-lg-4 mb-3 mb-lg-0">
+              <div class="panel d-flex h-100">
+                <div class="flex-shrink-0 pt-1">
+                  <img src="~/assets/img/icons/stock.svg" width="28" height="21" alt="">
+                </div>
+                <div class="flex-grow-1 ms-3">
+                  <p class="mb-0 fs-18 fw-medium">
+                    {{language[current][5]}}
+                  </p>
                 </div>
               </div>
             </div>
           </div>
-        </section>
-        <section id="about" class="about section">
-          <div class="container">
-            <div class="row">
-              <div class="col-lg-5 mb-3 mb-lg-0">
-                <h2 class="section-title">
-                  {{language[current][1]}}
-                </h2>
-                <p class="section-paragraph">
-                  {{language[current][6]}}
-                </p>
-                <p class="section-paragraph">
-                  {{language[current][7]}}
-                </p>
-                <p class="section-paragraph">
-                  {{language[current][8]}}
-                </p>
-                <p class="section-paragraph">
-                  {{language[current][9]}}
-                </p>
-              </div>
-              <div class="col-lg-7">
-                <AboutPanel
-                  v-for="item in aboutItems[current]"
-                  :key="item.id"
-                  :icon="item.iconUrl"
-                  class="mb-3"
-                >
-                  {{ item.title }}
-                </AboutPanel>
-              </div>
+        </div>
+      </section>
+      <section id="about" class="about section">
+        <div class="container">
+          <div class="row">
+            <div class="col-lg-5 mb-3 mb-lg-0">
+              <h2 class="section-title">
+                {{language[current][1]}}
+              </h2>
+              <p class="section-paragraph">
+                {{language[current][6]}}
+              </p>
+              <p class="section-paragraph">
+                {{language[current][7]}}
+              </p>
+              <p class="section-paragraph">
+                {{language[current][8]}}
+              </p>
+              <p class="section-paragraph">
+                {{language[current][9]}}
+              </p>
+            </div>
+            <div class="col-lg-7">
+              <AboutPanel
+                v-for="item in aboutItems[current]"
+                :key="item.id"
+                :icon="item.iconUrl"
+                class="mb-3"
+              >
+                {{ item.title }}
+              </AboutPanel>
             </div>
           </div>
-        </section>
-        <section id="infrastructure" class="infrastructure section">
-          <div class="container">
-            <div class="row">
-              <div class="col-lg-5 text-white mb-3 mb-lg-0">
-                <h2 class="section-title">
-                  {{language[current][2]}}
-                </h2>
-                <p class="section-paragraph">
-                  {{language[current][10]}}
-                </p>
-                <p class="section-paragraph">
-                  {{language[current][11]}}
-                </p>
-                <p class="section-paragraph">
-                  {{language[current][12]}}
-                </p>
-              </div>
-              <div class="col-lg-7">
-                <div class="row">
-                  <div v-for="item in infrastructureItems[current]" :key="item.id" class="col-md-6 mb-3">
-                    <InfrastructurePanel :icon="item.iconUrl" class="h-100">
-                      {{ item.title }}
-                    </InfrastructurePanel>
-                  </div>
+        </div>
+      </section>
+      <section id="infrastructure" class="infrastructure section">
+        <div class="container">
+          <div class="row">
+            <div class="col-lg-5 text-white mb-3 mb-lg-0">
+              <h2 class="section-title">
+                {{language[current][2]}}
+              </h2>
+              <p class="section-paragraph">
+                {{language[current][10]}}
+              </p>
+              <p class="section-paragraph">
+                {{language[current][11]}}
+              </p>
+              <p class="section-paragraph">
+                {{language[current][12]}}
+              </p>
+            </div>
+            <div class="col-lg-7">
+              <div class="row">
+                <div v-for="item in infrastructureItems[current]" :key="item.id" class="col-md-6 mb-3">
+                  <InfrastructurePanel :icon="item.iconUrl" class="h-100">
+                    {{ item.title }}
+                  </InfrastructurePanel>
                 </div>
               </div>
             </div>
           </div>
-        </section>
-        <section class="services section">
-          <div class="container">
-            <div class="row">
-              <div class="col-lg-8">
-                <h2 class="section-title">
-                  {{language[current][13]}}
-                </h2>
-              </div>
-            </div>
-            <div class="row">
-              <div v-for="item in serviceItems[current]" :key="item.id" class="col-lg-3 col-md-6 mb-3">
-                <ServicePanel :img="item.imgUrl" class="h-100">
-                  {{ item.title }}
-                </ServicePanel>
-              </div>
+        </div>
+      </section>
+      <section class="services section">
+        <div class="container">
+          <div class="row">
+            <div class="col-lg-8">
+              <h2 class="section-title">
+                {{language[current][13]}}
+              </h2>
             </div>
           </div>
-        </section>
-      </main>
-      <TheFooter :dark="$route.name !== 'index'" />
-    </vue-custom-scrollbar>
+          <div class="row">
+            <div v-for="item in serviceItems[current]" :key="item.id" class="col-lg-3 col-md-6 mb-3">
+              <ServicePanel :img="item.imgUrl" class="h-100">
+                {{ item.title }}
+              </ServicePanel>
+            </div>
+          </div>
+        </div>
+      </section>
+    </main>
+    <TheFooter :dark="$route.name !== 'index'" />
+    <style>
+      body {
+        overflow: auto !important;
+      }
+    </style>
   </div>
 </template>
 
@@ -185,8 +186,15 @@ export default {
       return this.$store.state.localStorage.current;
     }
   },
+  methods: {
+    scrollHandle(evt) {
+      let el  = document.getElementById('scroll');
+      console.log(el.scrollTop);
+    }
+  },
   data () {
     return {
+      ps: null,
       language: [
         ['Арендаторам','О нас','Инфраструктура','1 000 000+ офлайн-посетителей и 500 000+ онлайн-посетителей в год','1 200+ специализированных бутиков. Лучшие продавцы с уникальным ассортиментом','2 000 000+ наименований запчастей от ведущих мировых брендов','ТЦ «Car City» - крупнейший офлайн торговый центр в Средней Азии.','У нас вы найдёте большой ассортимент запчастей от ведущих мировых брендов, от конвейерных поставщиков до качественных аналогов. На европейские, японские, российские, корейские, китайские и прочие иномарки.','Наша философия основана на сохранении времени и денег покупателей, чтобы они могли жить лучше. Это сделало нас самым успешным ритейлером автомобильных запчастей в Казахстане.','Для любого автовладельца ТЦ «Car City» является синонимом низкой цены, гарантии и качества!','На территории торгового центра расположились сотни магазинов по продаже автозапчастей и\n' +
         '                  аксессуаров для любых марок, банк, уютные кофейни, банкоматы, терминалы и многое другое. Car City\n' +
@@ -260,6 +268,9 @@ export default {
   }
 }
 </script>
-<style lang="scss">
+<style lang="scss" scoped>
+  body {
+    overflow: auto !important;
+  }
   @import '/assets/scss/main.scss';
 </style>
