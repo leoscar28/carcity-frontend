a6b648d041f13e56c48f10d9fd6a14693b9dbc91
era <admin@smartproperty.kz>
2022-04-14 19:51:29 +0600

    optimized request loading

--- pages/instruction/index.vue
+++ pages/instruction/index.vue
@@ -13,7 +13,10 @@
                 <div class="rule-content-header-title">Инструкции</div>
               </div>
               <div class="rule-content-video">
-                <iframe width="560" height="315" src="https://www.youtube.com/embed/ZVSAh1tm5h0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                <iframe width="560" height="315" src="https://www.youtube.com/embed/rpJzNjR06PA" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+              </div>
+              <div class="rule-content-video">
+                <iframe width="560" height="315" src="https://www.youtube.com/embed/CTgInlnbq54" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
               </div>
             </div>
           </div>
