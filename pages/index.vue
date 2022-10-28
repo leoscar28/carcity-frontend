d9c52b623bbfde611f132c5e94faeeb7b7c738c5
“Akzhan <akzhan02@gmail.com>
2022-10-28 13:48:41 +0600

    awards slider

--- pages/index.vue
+++ pages/index.vue
@@ -106,16 +106,34 @@
               </h2>
             </div>
           </div>
-          <div class="row">
-            <div v-for="(item,key) in awards" class="col-lg-3 col-md-6 mb-3" :key="key">
-              <ServicePanel :img="item.img" class="h-100">
-                <template v-if="current === 0">
-                  {{ item.title }}
-                </template>
-                <template v-else>
-                  {{ item.title_kz }}
-                </template>
-              </ServicePanel>
+          <div id="awards-slider" class="glide">
+            <div class="glide__track" data-glide-el="track">
+              <ul class="glide__slides">
+                <li class="glide__slide" v-for="(item,key) in awards" :key="key">
+                  <div class="col-lg-12 mb-3">
+                    <ServicePanel :img="item.img" class="h-100">
+                      <template v-if="current === 0">
+                        {{ item.title }}
+                      </template>
+                      <template v-else>
+                        {{ item.title_kz }}
+                      </template>
+                    </ServicePanel>
+                  </div>
+                </li>
+              </ul>
+            </div>
+            <div class="glide__arrows glide__arrows--circle" data-glide-el="controls">
+              <button class="glide__arrow glide__arrow--left" data-glide-dir="<">
+                <svg width="13" height="20" viewBox="0 0 13 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+                  <path d="M12.3985 2.7325L10.166 0.5L0.666016 10L10.166 19.5L12.3985 17.2675L5.14685 10L12.3985 2.7325Z" fill="currentColor" />
+                </svg>
+              </button>
+              <button class="glide__arrow glide__arrow--right" data-glide-dir=">">
+                <svg width="13" height="20" viewBox="0 0 13 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+                  <path d="M0.601483 2.7325L2.83398 0.5L12.334 10L2.83398 19.5L0.601483 17.2675L7.85315 10L0.601483 2.7325Z" fill="currentColor" />
+                </svg>
+              </button>
             </div>
           </div>
         </div>
@@ -193,7 +211,9 @@ import ServicePanel from '/components/site/ServicePanel';
 import TheFooter from "/components/site/TheFooter";
 import vueCustomScrollbar from 'vue-custom-scrollbar'
 import "vue-custom-scrollbar/dist/vueScrollbar.css"
-import UserBannerSection from "../components/UserBannerSection";
+import UserBannerSection from "../components/UserBannerSection"
+import Glide from '@glidejs/glide'
+
 export default {
   name: 'IndexPage',
   components: {
@@ -270,6 +290,27 @@ export default {
       { property: 'og:description', content: 'Низкие цены. Гарантия качества. Отличный сервис.  Более 50 миллионов позиций - запчасти, масла, автохимия, электроника, аксессуары' },
       { property: 'og:locale', content: 'ru_RU' }
     ]
+  },
+  mounted() {
+    new Glide('#awards-slider', {
+      animationTimingFunc: 'ease-in-out',
+      animationDuration: 800,
+      autoplay: 4000,
+      perView: 4,
+      gap: 15,
+      type: 'carousel',
+      breakpoints: {
+        700: {
+          peek: { before: 0, after: 120 },
+          perView: 1,
+          focusAt: "center"
+        },
+        1000: {
+          peek: { before: 0, after: 120 },
+          perView: 2
+        }
+      }
+    }).mount()
   }
 }
 </script>
