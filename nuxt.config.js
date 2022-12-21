26334a67902eb39cb1a3d1dcd0984f61e17d45ea
leoscar28 <67303134+leoscar28@users.noreply.github.com>
2022-12-21 13:29:33 +0600

    Revert "Revert "Revert "Revert "let it snow""""
    This reverts commit fc731de03b673cada295f2293a6a348b1f6d26a9.

--- nuxt.config.js
+++ nuxt.config.js
@@ -36,7 +36,8 @@ export default {
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
     {src: '~/plugins/repository.js', ssr: false},
-    { src: '~/plugins/aos.js', mode: 'client' }
+    { src: '~/plugins/aos.js', mode: 'client' },
+    { src: '~/plugins/snow.js'},
   ],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
